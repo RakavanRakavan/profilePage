dd54a00a12cd356662070cd7e928cf40b0d98523
RakavanRakavan <rakavanrakavan1709@gmail.com>
2023-05-18 18:53:10 +0530

    project initial setup

---
+++ App.js
@@ -0,0 +1,38 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App() {
+
+  return (
+    <SafeAreaView style={styles.Container}>
+     <View>
+      <Text>dfgdfg</Text>
+     </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  Container: {
+   flex:1,
+   justifyContent:"center",
+   alignItems:"center"
+
+  },
+});
+
+export default App;
